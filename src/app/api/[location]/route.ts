24b5b21f073a1ed45bcb7b1253726c942eff10bc
Ankit Aggarwal <kuldeepaggarwal305@gmail.com>
2024-06-02 14:16:57 +0530

    some change

--- src/app/api/[location]/route.ts
+++ src/app/api/[location]/route.ts
@@ -4,8 +4,8 @@ import { NextRequest,NextResponse } from 'next/server';
 export async function GET(request: NextRequest, { params }: { params: { location: string } }) {
   const location = params.location; // Extract the dynamic parameter
 
-  const apiKey = process.env.NEXT_PUBLIC_WEATHER_API_KEY;
-  const url = `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${location}&days=6&aqi=yes&alerts=yes`;
+//   const apiKey = process.env.NEXT_PUBLIC_WEATHER_API_KEY;
+  const url = `http://api.weatherapi.com/v1/forecast.json?key=5f8554e2a21e417eae3100613243105&q=${location}&days=6&aqi=yes&alerts=yes`;
 
   try {
     const response = await fetch(url);
